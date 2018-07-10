fbbe2ca4c4d45d62cb3a33d80f1b761299dd7bca
Alex Ameen <alex.ameen.tx@gmail.com>
2018-07-09 19:02:14 -0500

    fixed the help flag

--- main.c
+++ main.c
@@ -32,7 +32,7 @@ main (int argc, char *argv[])
 
     if (argc > 1)
     {
-        if (strcmp (argv[1], "-h") || strcmp (argv[1], "--help"))
+        if (strcmp (argv[1], "-h") == 0 || strcmp (argv[1], "--help") == 0)
         {
             print_help ();
             return EXIT_SUCCESS;
